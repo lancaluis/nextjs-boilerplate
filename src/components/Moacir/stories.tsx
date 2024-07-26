de26bd96f3d9e569e6131bd719f0005477137351
Luís Lança <luis_lanca@outlook.com>
2024-07-26 14:31:37 +0300

    Adding CI file
    Signed-off-by: Luís Lança <luis_lanca@outlook.com>

--- src/components/Moacir/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Meta, StoryObj } from '@storybook/react'
-import Moacir from '.'
-
-export default {
-  title: 'Moacir',
-  component: Moacir
-} as Meta
-
-export const Default: StoryObj = {}
