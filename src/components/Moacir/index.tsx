09f8d0ffe18c6272ea60d1ebb1ae26791bf72bfd
Luís Lança <luis_lanca@outlook.com>
2024-07-26 14:14:23 +0300

    Adding generators to automate components
    Signed-off-by: Luís Lança <luis_lanca@outlook.com>

---
+++ src/components/Moacir/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Moacir = () => (
+  <S.Wrapper>
+    <h1>Moacir</h1>
+  </S.Wrapper>
+)
+
+export default Moacir
---
+++ src/components/Moacir/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Moacir from '.'
+
+export default {
+  title: 'Moacir',
+  component: Moacir
+} as Meta
+
+export const Default: StoryObj = {}
